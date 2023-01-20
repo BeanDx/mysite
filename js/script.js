923d8e1c889aba27ca2cd1920f792d53ac9aaf18
BeanD <111660326+BeanDx@users.noreply.github.com>
2023-01-20 21:06:24 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// ========================
+// Themes
+document.querySelector('.themetoggle').addEventListener('click', (event) => {
+    event.preventDefault();
+    if (localStorage.getItem('theme') === 'dark') {
+        localStorage.removeItem('theme');
+    } else {
+        localStorage.setItem('theme', 'dark');
+    }
+    addDarkClassHTML();
+});
+
+function addDarkClassHTML() {
+    try {
+        if (localStorage.getItem('theme') === 'dark') {
+            document.querySelector('html').classList.add('dark');
+            document.querySelector('.themetoggle span').textContent = 'dark_mode';
+        }
+        else {
+            document.querySelector('html').classList.remove('dark');
+            document.querySelector('.themetoggle span').textContent = 'wb_sunny';
+        }
+    } catch (err) { }
+}
+addDarkClassHTML();
+// ========================
